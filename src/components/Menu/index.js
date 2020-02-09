64d2a1e02123aec585d5b2aaf8e73cec044d4cd6
Yoan <y.n.dorny@gmail.com>
2020-02-09 20:10:13 +0100

    Menu component

---
+++ src/components/Menu/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { Links, MenuWrapper, SurfRiderRibbon, TopRibbon, LogoWrapper } from './styles'
+import SurfRiderLogo from '../../assets/images/logos/surfrider.svg'
+import Button from '../Button'
+
+const Menu = () => (
+  <MenuWrapper>
+    <SurfRiderRibbon>
+      <TopRibbon>
+        <span>
+          Proudly <br />
+          powered by
+        </span>
+      </TopRibbon>
+      <LogoWrapper>
+        <img src={SurfRiderLogo} alt="surfrider logo" />
+      </LogoWrapper>
+    </SurfRiderRibbon>
+    <Links>
+      <span>The project</span>
+      <span>Contribute</span>
+      <span>Data</span>
+      <span>About us</span>
+    </Links>
+    <Button label="Download the app" />
+  </MenuWrapper>
+)
+
+export default Menu
---
+++ src/components/Menu/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+
+const ribbonWidth = '100px'
+const menuHeight = '50px'
+
+export const MenuWrapper = styled.div`
+  height: ${menuHeight};
+  position: relative;
+  display: flex;
+  padding-left: calc(${ribbonWidth} + 16px * 2);
+  padding-right: 16px;
+  justify-content: space-between;
+  align-items: center;
+  max-width: ${props => props.maxWidth};
+`
+export const Links = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 16px;
+  span {
+    margin-right: 16px;
+    cursor: pointer;
+  }
+`
+
+export const SurfRiderRibbon = styled.div`
+  width: ${ribbonWidth};
+  font-family: 'Montserrat';
+  position: absolute;
+  left: 16px;
+  top: 0;
+  background: ${props => props.theme.surfRiderBlue};
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  border-radius: 0 0 3px 3px;
+`
+
+export const TopRibbon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: ${menuHeight};
+  margin: auto;
+  span {
+    color: white;
+    text-align: center;
+    font-size: ${props => props.theme.defaultFS};
+  }
+`
+
+export const LogoWrapper = styled.div`
+  width: 85px;
+  margin: auto auto 5px auto;
+`
