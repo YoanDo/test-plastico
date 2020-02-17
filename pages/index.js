35b4b4bd4ff32aad72e39009615b4f3221f05d41
Yoan <y.n.dorny@gmail.com>
2020-02-17 21:18:53 +0100

    first section wip

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import React, { Fragment } from 'react'
 import withLayout from '../src/components/Layout'
 import Header from '../src/components/Header'
 import { Wrapper } from '../src/components/Layout/styles'
+import OurMission from '../src/components/OurMission'
 
 const HomePage = () => (
   <Fragment>
     <Header />
+    <OurMission />
     <Wrapper>
       data <br />
       get involve
---
+++ src/components/OurMission/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Wrapper } from '../Layout/styles'
+import Title from '../Title'
+
+const OurMission = () => (
+  <Wrapper>
+    <Title />
+  </Wrapper>
+)
+export default OurMission
