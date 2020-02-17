a683f1f87bf028ea9421029310700b1a99920996
Yoan <y.n.dorny@gmail.com>
2020-02-17 21:17:32 +0100

    Breaker

--- src/components/Breaker/index.js
+++ src/components/Breaker/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { string, bool } from 'prop-types'
 
 import BreakerWrapper from './styles'
 
-const Breaker = () => <BreakerWrapper />
+const Breaker = ({ color, bold }) => <BreakerWrapper color={color} bold={bold} />
+
+Breaker.propTypes = {
+  color: string,
+  bold: bool,
+}
+
+Breaker.defaultProps = {
+  color: null,
+  bold: false,
+}
 
 export default Breaker
--- src/components/Breaker/styles.js
+++ src/components/Breaker/styles.js
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 
 const BreakerWrapper = styled.div`
   margin: 24px 0 40px;
-  height: 6px;
-  width: 32px;
-  background-color: ${props => props.theme.white};
+  height: ${props => (props.bold ? '10' : '6')}px;
+  width: 85px;
+  background-color: ${props => (props.color === 'black' ? props.theme.black : props.theme.white)};
 `
 
 export default BreakerWrapper
