72972def144ae7491f232820daa6da106366847f
Yoan <y.n.dorny@gmail.com>
2020-02-19 21:10:56 +0100

    React reveal

--- src/components/OurMission/index.js
+++ src/components/OurMission/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
+import Fade from 'react-reveal/Fade'
+
 import { Wrapper } from '../Layout/styles'
 import { About, AboutImage, Left, Section } from './styles'
-
 import Title from '../Title'
 
 const OurMission = () => (
@@ -10,13 +11,15 @@ const OurMission = () => (
     <Section>
       <Title />
       <Left>
-        <About>
-          "
-          <span>
-            <FormattedMessage id="about_us_1" />
-          </span>
-          <FormattedMessage id="about_us_2" />"
-        </About>
+        <Fade bottom>
+          <About>
+            "
+            <span>
+              <FormattedMessage id="about_us_1" />
+            </span>
+            <FormattedMessage id="about_us_2" />"
+          </About>
+        </Fade>
         <AboutImage />
       </Left>
     </Section>
