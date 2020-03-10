d0b03dcba1be5051410099ef53a32903bce488f1
Yoan <y.n.dorny@gmail.com>
2020-03-10 15:14:41 +0100

    Fix - wave shape

--- src/components/Wave/styles.js
+++ src/components/Wave/styles.js
@@ -1,7 +1,21 @@
 import styled from 'styled-components'
 
+import waveTop from '../../assets/svg/waveTop.svg'
+import waveBot from '../../assets/svg/waveBot.svg'
+
 export const WaveWrapper = styled.div`
   padding: ${props => (props.menu ? 0 : `calc(${props.theme.mainTitleFS}*2) 0 0 0`)};
+  width: 100vw;
+`
+
+export const WaveImgTop = styled.img`
+  content: url(${waveTop});
+  width: 100vw;
+`
+
+export const WaveImgBot = styled.img`
+  content: url(${waveBot});
+  width: 100vw;
 `
 
 export const PlainBlue = styled.div`
