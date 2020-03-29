3ae11022f7de5277bb32539b2fa72797433c7f8e
Yoan <y.n.dorny@gmail.com>
2020-03-29 19:34:27 +0200

    Navigation

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,38 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { isNil } from 'ramda'
+import { animateScroll as scroll } from 'react-scroll'
 
 import Menu from '../Menu'
 import GlobalStyle, { Wrapper } from './styles'
 
-const withLayout = Page => () => (
-  <Fragment>
-    <Wrapper menu>
-      <Menu />
-    </Wrapper>
-    <Page />
-    <GlobalStyle whiteColor />
-  </Fragment>
-)
+const withLayout = Page => () => {
+  const { target, offSet } = useRouter().query
+  const smoothScroll = () => {
+    if (isNil(target)) return null
+    const adjust = (!isNil(offSet) && offSet) || 0
+    const targetPosition = document.getElementsByName(target)[0].offsetTop - adjust
+    scroll.scrollTo(targetPosition, {
+      activeClass: true,
+      duration: 500,
+      delay: 100,
+      smooth: true,
+    })
+  }
+
+  useEffect(() => {
+    smoothScroll()
+  })
+
+  return (
+    <Fragment>
+      <Wrapper menu>
+        <Menu />
+      </Wrapper>
+      <Page />
+      <GlobalStyle whiteColor />
+    </Fragment>
+  )
+}
 
 export default withLayout
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import * as Scroll from 'react-scroll'
-import { Link } from 'react-scroll'
+import Link from 'next/link'
 
 import { Links, LogoWrapper, MenuWrapper, RightWrapper, SocialWrapper, SurfRiderRibbon, TopRibbon } from './styles'
 import SurfRiderLogo from '../../assets/images/logos/surfrider.svg'
@@ -21,13 +20,15 @@ const Menu = () => (
       </LogoWrapper>
     </SurfRiderRibbon>
     <Links>
-      <Link activeClass="active" to="project" spy smooth duration={500} offset={100}>
+      <Link href={{ pathname: '/', query: { target: 'project', offSet: -100 } }}>
         <span>The project</span>
       </Link>
-      <Link activeClass="active" to="getInvolved" spy smooth duration={500} offset={-100}>
+      <Link href={{ pathname: '/', query: { target: 'getInvolved', offSet: 100 } }}>
         <span>Contribute</span>
       </Link>
-      <span>Data</span>
+      <Link href="/data">
+        <span>Data</span>
+      </Link>
       <span>About us</span>
     </Links>
     <RightWrapper>
