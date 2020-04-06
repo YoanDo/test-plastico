ed806772ee6aa34afea2075149992fd162d44e37
Yoan <y.n.dorny@gmail.com>
2020-04-06 12:40:42 +0200

    scroll - dirty trick

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,35 +1,42 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import Fade from 'react-reveal/Fade'
+import { useMediaQuery } from 'react-responsive'
 
 import { Wrapper } from '../Layout/styles'
 import { About, AboutImage, Left, Section } from './styles'
 import Title from '../Title'
 import Goals from '../Goals'
 
-const Project = () => (
-  <Wrapper>
-    <Section>
-      <Title color={props => props.theme.black}>
-        <FormattedMessage id="our_mission_1" /> <br />
-        <FormattedMessage id="our_mission_2" />
-      </Title>
-      <Left name="project">
-        <Fade bottom>
-          <About>
-            "
-            <span>
-              <FormattedMessage id="about_us_1" />
-            </span>
-            <FormattedMessage id="about_us_2" />"
-          </About>
-        </Fade>
-        <AboutImage />
-      </Left>
-    </Section>
-    <Section>
-      <Goals />
-    </Section>
-  </Wrapper>
-)
+const Project = () => {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 900px)' })
+
+  return (
+    <Wrapper>
+      {isTabletOrMobile && <span name="project" />}
+      <Section>
+        <Title color={props => props.theme.black}>
+          <FormattedMessage id="our_mission_1" /> <br />
+          <FormattedMessage id="our_mission_2" />
+        </Title>
+        <Left>
+          {!isTabletOrMobile && <span name="project" />}
+          <Fade bottom>
+            <About>
+              "
+              <span>
+                <FormattedMessage id="about_us_1" />
+              </span>
+              <FormattedMessage id="about_us_2" />"
+            </About>
+          </Fade>
+          <AboutImage />
+        </Left>
+      </Section>
+      <Section>
+        <Goals />
+      </Section>
+    </Wrapper>
+  )
+}
 export default Project
