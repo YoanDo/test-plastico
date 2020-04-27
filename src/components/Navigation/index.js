b617d37069abfb3f99752099b2d3d1235023d28e
Yoan <y.n.dorny@gmail.com>
2020-04-27 16:29:01 +0200

    Feat - Navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { func } from 'prop-types'
+import { func, bool, string } from 'prop-types'
 
 import { Links } from './styles'
 
-const NavigationLinks = ({ closePanel }) => (
-  <Links onClick={() => closePanel()}>
+const NavigationLinks = ({ align, closePanel, column, burger }) => (
+  <Links column={column} burger={burger} align={align} onClick={() => closePanel()}>
     <Link href={{ pathname: '/', query: { target: 'project' } }}>
       <span>The project</span>
     </Link>
@@ -22,10 +22,16 @@ const NavigationLinks = ({ closePanel }) => (
 
 NavigationLinks.propTypes = {
   closePanel: func,
+  column: bool,
+  burger: bool,
+  align: string,
 }
 
 NavigationLinks.defaultProps = {
-  closePanel: null,
+  align: 'center',
+  closePanel: Function.prototype,
+  column: false,
+  burger: false,
 }
 
 export default NavigationLinks
--- src/components/Navigation/styles.js
+++ src/components/Navigation/styles.js
@@ -3,11 +3,11 @@ import styled from 'styled-components'
 // Links
 export const Links = styled.div`
   display: flex;
-  align-items: center;
   padding-right: 16px;
+  flex-direction: ${props => (props.column ? 'column' : 'row')};
+  align-items: ${props => props.align};
   @media (max-width: 900px) {
     flex-direction: column;
-    align-items: center;
     padding: 0;
   }
   span {
@@ -16,10 +16,11 @@ export const Links = styled.div`
     cursor: pointer;
     color: ${props => props.theme.black};
     @media (max-width: 900px) {
-      font-size: ${props => props.theme.fourthFS};
-      margin-bottom: ${props => props.theme.fourthFS};
+      font-size: ${props => (props.burger ? props.theme.fourthFS : props.theme.defaultFS)};
+      margin-bottom: ${props => (props.burger ? props.theme.fourthFS : 'inherit')};
+      line-height: 1.6;
       margin-right: 0;
-      font-family: 'Bebas Neue';
+      font-family: ${props => (props.burger ? 'Bebas Neue' : 'inherit')};
     }
   }
 `
