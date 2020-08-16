7a9947eaad5527b653394a2a603f79c4ca2d5e8b
Yoan <y.n.dorny@gmail.com>
2020-08-16 11:09:50 +0200

    Api - test

--- src/components/Header/components/KeyDatas/index.js
+++ src/components/Header/components/KeyDatas/index.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import Data from './Data'
 
 import { Datas } from './styles.js'
 
-const KeyDatas = () => (
-  <Datas>
-    <Data label="contributor" value={870} timing={4} />
-    <Data label="km" value={3456} timing={5} />
-    <Data label="trash_per_km" value={20} timing={3} />
-  </Datas>
-)
+const KeyDatas = () => {
+  const getDatas = () => {
+    axios
+      .get('https://plasticostorageprod.blob.core.windows.net/public/data_home_page.json')
+      .then(res => console.log('api:', res))
+      .catch(er => console.log('api:', er))
+  }
+
+  // useEffect(() => console.log(getDatas()))
+  getDatas()
+
+  return (
+    <Datas>
+      <Data label="contributor" value={870} timing={4} />
+      <Data label="km" value={3456} timing={5} />
+      <Data label="trash_per_km" value={20} timing={3} />
+    </Datas>
+  )
+}
 
 export default KeyDatas
