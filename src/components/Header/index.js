c8a44e0f8db04d0375036488674865bf8c2c04a7
Yoan <Y.N.dorny@gmail.com>
2020-12-09 19:17:05 +0100

    feat(landing): add welcome animation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useMediaQuery } from 'react-responsive'
-
-import Breaker from '../UI/Breaker'
-import KeyDatas from './components/KeyDatas'
-import { Banner, Title, MobileBanner } from './styles'
+import { Banner, MobileBanner, Title } from './styles'
+import gsap from 'gsap'
+import { animateScroll as scroll } from 'react-scroll'
 
 import Ribbon from '../Ribbon'
 
 const Header = () => {
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 900px)' })
+  const ref = useRef(null)
+
+  function scrollMoreDown300() {
+    const isUserTop = window && window.pageYOffset === 0
+    isUserTop && scroll.scrollMore(ref.current.clientHeight)
+  }
+
+  useEffect(() => {
+    const tl = gsap.timeline()
+    tl.from('#banner', { autoAlpha: '0', duration: 2, ease: 'power3.in' })
+    tl.from('#title', { autoAlpha: '0', duration: 1 })
+    tl.add(function () {
+      scrollMoreDown300()
+    }, '+=1')
+  }, [])
 
   return (
-    <Banner>
+    <Banner id={'banner'} ref={ref}>
       <MobileBanner>
         {isTabletOrMobile && <Ribbon right="20px" top="0" left="auto" />}
-        <Title>Plastic Origins</Title>
+        <Title id={'title'}>Plastic Origins</Title>
       </MobileBanner>
-      <Breaker />
-      <KeyDatas />
     </Banner>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,14 +3,14 @@ import styled from 'styled-components'
 import background from '../../assets/images/headerBg.jpg'
 
 export const Banner = styled.div`
-  height: 100vh;
+  align-items: center;
+  background-image: url(${background});
   background-position: center;
   background-size: cover;
-  background-image: url(${background});
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: column;
+  height: 100vh;
+  justify-content: center;
   @media (max-width: 900px) {
     height: 100%;
     background-image: none;
@@ -21,19 +21,6 @@ export const Title = styled.h1`
   color: ${(props) => props.theme.white};
   text-transform: uppercase;
   font-size: ${(props) => props.theme.mainTitleFS};
-  letter-spacing: 1.93px;
-  @media (max-width: 900px) {
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
-`
-
-export const Claim = styled.h2`
-  display: flex;
-  color: ${(props) => props.theme.white};
-  text-transform: uppercase;
-  font-size: ${(props) => props.theme.secondaryFS};
   letter-spacing: 1.93px;
   @media (max-width: 900px) {
     justify-content: center;
