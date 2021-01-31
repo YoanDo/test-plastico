819030749fbb6ae7d84d290d1b5821abd859156e
Yoan <y.n.dorny@gmail.com>
2021-01-31 18:43:25 +0100

    feat(getInvalved): update links

--- src/components/GetInvolved/index.js
+++ src/components/GetInvolved/index.js
@@ -4,19 +4,30 @@ import Title from '../Title'
 import Cart from '../Cart'
 import { CartWrapper, Wrapper } from './styles'
 
-const GetInvolved = () => (
-  <Wrapper>
-    <Title>
-      <FormattedMessage id="get_involved_1" />
-      <br />
-      <FormattedMessage id="get_involved_2" />
-    </Title>
-    <CartWrapper>
-      <Cart label="get_involved_collect" link="mailto:plasticorigins@surfrider.eu" />
-      <Cart label="get_involved_label" link="mailto:plasticorigins@surfrider.eu" />
-      <Cart label="get_involved_donate" marginBottom={false} link="https://donate.surfrider.eu/b/mon-don" />
-    </CartWrapper>
-  </Wrapper>
-)
+const GetInvolved = () => {
+  const isUserFrench =
+    typeof window !== 'undefined' &&
+    typeof window.navigator !== 'undefined' &&
+    navigator.language &&
+    navigator.language.split(/[-_]/)[0] === 'fr'
+
+  const zoomLinkEN = 'https://us02web.zoom.us/meeting/register/tZ0rcOCqqDkqHtP_hcUF0lI3avfpi10DuTNq'
+  const zoomLinkFR = 'https://us02web.zoom.us/meeting/register/tZ0rc-utpzgqGdR0Kn3NDRgFuq4saddL9ugz'
+
+  return (
+    <Wrapper>
+      <Title>
+        <FormattedMessage id='get_involved_1' />
+        <br />
+        <FormattedMessage id='get_involved_2' />
+      </Title>
+      <CartWrapper>
+        <Cart label='get_involved_collect' link={isUserFrench ? zoomLinkFR : zoomLinkEN} />
+        <Cart label='get_involved_label' link='http://trashroulette.com' />
+        <Cart label='get_involved_donate' marginBottom={false} link='https://donate.surfrider.eu/b/mon-don' />
+      </CartWrapper>
+    </Wrapper>
+  )
+}
 
 export default GetInvolved
