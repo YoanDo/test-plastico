4b3cb5e5a63756c57528c8f4fbd1d1ad231f8853
Yoan <y.n.dorny@gmail.com>
2021-02-01 19:43:25 +0100

    refactor(home): dynamic import

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
 import React, { Fragment } from 'react'
+import dynamic from 'next/dynamic'
 
 import Footer from '../src/components/Footer'
 import Header from '../src/components/Header'
-import Project from '../src/components/Project'
-import Video from '../src/components/Video'
 import Wave from '../src/components/Wave'
 import withLayout from '../src/hoc/layout'
 import KeyDatas from '../src/components/KeyDatas'
 
-const HomePage = () => (
-  <Fragment>
-    <Header />
-    <KeyDatas />
-    <Project />
-    <Video />
-    <Wave />
-    <Footer />
-  </Fragment>
-)
+function HomePage() {
+  const Project = dynamic(import('../src/components/Project'))
+  const Video = dynamic(import('../src/components/Video'))
+
+  return (
+    <Fragment>
+      <Header />
+      <KeyDatas />
+      <Project />
+      <Video />
+      <Wave />
+      <Footer />
+    </Fragment>
+  )
+}
 export default withLayout(HomePage)
