2c558194fcaa5964250ecfb142fe1655b220aef0
Yoan <y.n.dorny@gmail.com>
2021-02-08 21:53:52 +0100

    feat(team): TeamCard component

---
+++ src/components/TeamList/components/TeamCard/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { string } from 'prop-types'
+
+import { Color, IdentityRow, Picture, PictureWrapper, Position, Wrapper } from './styles'
+import { useIntl } from 'react-intl'
+
+const TeamCard = ({ link, name, position, surname }) => {
+  const imageLink = require(`../../../../assets/images/team_profiles/${name}${surname}.jpg`)
+  const positionTrad = useIntl().formatMessage({ id: position })
+  if (!imageLink && !positionTrad) return null
+
+  return (
+    <Wrapper href={link} target="_blank" rel="noopener noreferrer">
+      <PictureWrapper>
+        <Picture backgroundUrl={imageLink} />
+        <Color />
+      </PictureWrapper>
+      <IdentityRow>
+        <span>{name}</span>
+        <span>{surname}</span>
+      </IdentityRow>
+      <Position>{positionTrad}</Position>
+    </Wrapper>
+  )
+}
+TeamCard.propTypes = {
+  link: string.isRequired,
+  name: string.isRequired,
+  position: string.isRequired,
+  surname: string.isRequired,
+}
+
+TeamCard.defaultProps = {
+  link: null,
+  name: null,
+  position: null,
+  surname: null,
+}
+export default TeamCard
