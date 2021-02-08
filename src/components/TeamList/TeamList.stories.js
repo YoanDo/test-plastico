33d3cef287775dfbc4957cfcd709bdb1b0f7ce9c
Yoan <y.n.dorny@gmail.com>
2021-02-08 21:55:28 +0100

    feat(team): TeamList component

---
+++ src/components/TeamList/TeamList.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TeamList from '.'
+
+export default {
+  title: 'Component/TeamList',
+  component: TeamList,
+  argTypes: {},
+}
+
+export const defaultTeamList = () => {
+  return <TeamList />
+}
---
+++ src/components/TeamList/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+`
+export const CardWrapper = styled.div`
+  margin: 0 auto;
+  margin-bottom: 25px;
+`
