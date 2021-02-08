a6fb945ef781cfc4d604012dfd7768a7101b410f
Yoan <y.n.dorny@gmail.com>
2021-02-08 21:48:55 +0100

    feat(team): add trad, constant & pictures

---
+++ src/constants/team.js
@@ -0,0 +1,17 @@
+export const PO_TEAM = [
+  {
+    name: 'Antoine',
+    surname: 'Bruge',
+    position: 'founder',
+    link: 'https://www.linkedin.com/in/antoinebruge/?originalSubdomain=fr',
+  },
+  { name: 'Yoan', surname: 'Dorny', position: 'dev_ux', link: 'http://www.yoan-dorny.com/' },
+  {
+    name: 'Christophe',
+    surname: 'Havard',
+    position: 'db',
+    link: 'https://www.linkedin.com/in/christophe-havard-b38b4849/',
+  },
+  { name: 'Marjorie', surname: 'Darcet', position: 'pm', link: 'https://www.linkedin.com/in/marjoriedarcet/' },
+  { name: 'Olivier', surname: 'Large', position: 'dev', link: 'https://fr.linkedin.com/in/olivier-large-4a0ba320' },
+]
