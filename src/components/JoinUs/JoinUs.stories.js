72c59fe288dabb2e39ecbe9a0a3c75c5d1b26ee4
Yoan <y.n.dorny@gmail.com>
2021-02-10 17:54:57 +0100

    feat(team): JoinUs component

---
+++ src/components/JoinUs/JoinUs.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import JoinUs from '.'
+
+export default {
+  title: 'Component/JoinUs',
+  component: JoinUs,
+  argTypes: {},
+}
+
+export const defaultJoinUs = () => {
+  return <JoinUs />
+}
---
+++ src/components/JoinUs/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+
+import { SponsorsWrapper } from './styles'
+import { Wrapper } from '../../hoc/layout/styles'
+import Button from '../UI/Button'
+
+const JoinUs = () => (
+  <SponsorsWrapper>
+    <Wrapper>
+      <span>
+        <FormattedMessage id="join_team_1" />
+        <br />
+        <FormattedMessage id="join_team_2" />
+      </span>
+      <a href="mailto:plasticorigins@surfrider.eu">
+        <Button label="join_team_cta" negative />
+      </a>
+    </Wrapper>
+  </SponsorsWrapper>
+)
+
+export default JoinUs
