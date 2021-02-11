7266b5c585409994881cc4548ca92bdbfcc396fd
Yoan <y.n.dorny@gmail.com>
2021-02-11 09:59:00 +0100

    fix(teamlist): bad shuffling of members

--- src/components/TeamList/index.js
+++ src/components/TeamList/index.js
@@ -1,20 +1,23 @@
 import React from 'react'
 
 import { PO_TEAM } from '../../constants/team'
-import shuffle from '../../helpers/shuffle'
 import { Wrapper, CardWrapper } from './styles'
 import dynamic from 'next/dynamic'
 
 const TeamCard = dynamic(import('./components/TeamCard'))
 
-const TeamList = () => (
-  <Wrapper>
-    {shuffle(PO_TEAM).map(({ name, surname, position, link }) => (
-      <CardWrapper key={link}>
-        <TeamCard name={name} surname={surname} position={position} link={link} />
-      </CardWrapper>
-    ))}
-  </Wrapper>
-)
+const TeamList = () => {
+  const shuffledArrayOfMembers = PO_TEAM.sort(() => Math.random() - 0.5)
+
+  return (
+    <Wrapper>
+      {shuffledArrayOfMembers.map(({ name, surname, position, link }) => (
+        <CardWrapper key={link}>
+          <TeamCard name={name} surname={surname} position={position} link={link} />
+        </CardWrapper>
+      ))}
+    </Wrapper>
+  )
+}
 
 export default TeamList
