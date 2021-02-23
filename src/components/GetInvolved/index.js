c97356e5470931dc91a221f58aad21a889d1cc82
Yoan <y.n.dorny@gmail.com>
2021-02-23 09:37:11 +0100

    refactor(getInvolve): update link & refactor style

--- src/components/GetInvolved/index.js
+++ src/components/GetInvolved/index.js
@@ -2,24 +2,24 @@ import React from 'react'
 import { FormattedMessage, useIntl } from 'react-intl'
 import Title from '../Title'
 import Cart from '../Cart'
-import { CartWrapper, Wrapper } from './styles'
+import { CartWrapper, BlueWrapper } from './styles'
 
 const GetInvolved = () => {
   const zoomLink = useIntl().formatMessage({ id: 'get_involve_zoom_link' })
 
   return (
-    <Wrapper>
-      <Title>
+    <BlueWrapper>
+      <Title color="white">
         <FormattedMessage id="get_involved_1" />
         <br />
         <FormattedMessage id="get_involved_2" />
       </Title>
       <CartWrapper>
         <Cart label="get_involved_collect" link={zoomLink} />
-        <Cart label="get_involved_label" link="http://trashroulette.com" />
+        <Cart label="get_involved_label" link="http://www.trashroulette.com" />
         <Cart label="get_involved_donate" marginBottom={false} link="https://donate.surfrider.eu/b/mon-don" />
       </CartWrapper>
-    </Wrapper>
+    </BlueWrapper>
   )
 }
 
--- src/components/GetInvolved/styles.js
+++ src/components/GetInvolved/styles.js
@@ -1,14 +1,20 @@
 import styled from 'styled-components'
+import { Wrapper } from '../../hoc/layout/styles'
 
 export const CartWrapper = styled.div`
   display: flex;
   flex-direction: column;
 `
 
-export const Wrapper = styled.div`
+export const BlueWrapper = styled(Wrapper)`
+  padding-top: 0;
+  margin-top: 0;
   display: flex;
   max-width: ${(props) => props.theme.maxWidth};
   align-items: center;
+  @media (max-width: 1280px) {
+    margin-top: 0;
+  }
   @media (max-width: 900px) {
     flex-direction: column;
     padding: 0 ${(props) => props.theme.spacing(1)};
