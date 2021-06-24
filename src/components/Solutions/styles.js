8437f453f0a6a772580caad990e289960f1c4ecf
“Yoan <y.n.dorny@gmail.com>
2021-06-24 20:50:25 +0200

    style(solutions): min height

--- src/components/Solutions/styles.js
+++ src/components/Solutions/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const SolutionsWrapper = styled.div`
   width: 100%;
+  min-height: 400px;
   display: flex;
   flex-direction: row;
   overflow: hidden;
