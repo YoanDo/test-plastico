92969d77a7ac802e448f8517bbf765974bf79429
Yoan <y.n.dorny@gmail.com>
2022-03-15 22:44:37 +0100

    feat(landing): add text to antoine video

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,21 +1,32 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl'
 
 import { Wrapper } from '../../hoc/layout/styles'
-import { VideoWrapper } from './styles'
+import { LeftSide, RightSide, Row, VideoWrapper } from './styles'
 
 const Goals = () => (
   <Wrapper>
-    <VideoWrapper>
-      <iframe
-        title="plastic origins"
-        width="100%"
-        height="100%"
-        src="https://www.youtube.com/embed/gJiBng0Mo_k?controls=0"
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
-    </VideoWrapper>
+    <Row>
+    <LeftSide>
+        <VideoWrapper>
+          <iframe
+            title="plastic origins"
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/gJiBng0Mo_k?controls=0"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </VideoWrapper>
+      </LeftSide>
+      <RightSide>
+        <span>
+        <FormattedMessage id="video_antoine" />
+        </span>
+      </RightSide>
+      
+    </Row>
   </Wrapper>
 )
 export default Goals
