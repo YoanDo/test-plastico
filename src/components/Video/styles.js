d8c805766895448496a384a4d9a56c026d96fdda
Yoan <y.n.dorny@gmail.com>
2022-04-11 21:06:24 +0200

    styles(antoine-video): row

--- src/components/Video/styles.js
+++ src/components/Video/styles.js
@@ -1,8 +1,20 @@
 import styled from 'styled-components'
 
+export const Row = styled.div`
+  display: flex ;
+  flex-direction: row;
+  span{
+    font-size: ${({ theme }) => theme.biggerFS};
+  }
+  @media screen and (max-width: 992px) {
+    flex-direction: column;
+  }
+`
+
 export const VideoWrapper = styled.div`
   align-items: center;
   display: flex;
+  flex-direction: row;
   height: 0;
   justify-content: center;
   margin: auto;
@@ -16,5 +28,36 @@ export const VideoWrapper = styled.div`
     top: 0;
     width: 100%;
     height: 100%;
+  }
+`
+export const LeftSide = styled.div`
+  display: flex;
+  flex-basis: 50%;
+  height: 100%;
+  @media screen and (max-width: 992px) {
+    flex-basis: 100%;
+  }
+`
+
+export const RightSide = styled.div`
+  align-items: center;
+  display: flex;
+  flex-basis: 50%;
+  height: 100%;
+  justify-content: center;
+  margin: auto 0;
+  padding: ${({ theme }) => theme.spacing(1)};
+  width: 100%;
+  span{
+    width: 80%;
+    display: inline-block
+  }
+  @media screen and (max-width: 992px) {
+    flex-basis: 100%;
+    display: content ;
+    span{
+      width: 100%;
+      margin-top: ${({theme}) => theme.spacing(2)};
+    }
   }
 `
