05ee9747dbaf511deb607adea950450d4fc6b8a1
Yo <y.n.dorny@gmail.com>
2022-09-27 00:38:25 +0200

    feat(join-page): import components

--- src/scenes/Join/index.js
+++ src/scenes/Join/index.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react'
-import { FormattedMessage } from 'react-intl'
+import JoinPdf from '../../components/JoinPDF'
 import JoinUsHeader from '../../components/JoinUsHeader'
+import JoinVisio from '../../components/JoinVisio'
+import JoinVideo from '../../containers/JoinVideo'
 import { Wrapper } from '../../hoc/layout/styles'
 
-const JoinUsPage = () => {
-  
-  return (
-    <>
+const JoinUsPage = () => (
+  <>
     <Wrapper>
       <JoinUsHeader />
+      <JoinVisio />
+      <JoinVideo />
+      <JoinPdf />
     </Wrapper>
-    </>
-  )
-}
+  </>
+)
+
 export default JoinUsPage
