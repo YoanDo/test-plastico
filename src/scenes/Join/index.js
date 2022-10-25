795de79e3c7a777ca3336b1eb80cc84ea446dd66
Yo <y.n.dorny@gmail.com>
2022-10-25 22:15:09 +0200

    refactor(join-us): clean page

--- src/scenes/Join/index.js
+++ src/scenes/Join/index.js
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react'
 import JoinPdf from '../../components/JoinPDF'
 import JoinUsHeader from '../../components/JoinUsHeader'
-import JoinVisio from '../../components/JoinVisio'
+import JoinActions from '../../components/JoinActions'
 import JoinVideo from '../../containers/JoinVideo'
 import { Wrapper } from '../../hoc/layout/styles'
 
-const JoinUsPage = () => (
-  <>
-    <Wrapper>
-      <JoinUsHeader />
-      <JoinVisio />
-      <JoinVideo />
-      <JoinPdf />
-    </Wrapper>
-  </>
-)
+
+const JoinUsPage = () => {
+  return (
+    <>
+      <Wrapper>
+        <JoinUsHeader />
+        <JoinActions />
+        {/* <JoinVideo /> */}
+        {/* <JoinPdf /> */}
+      </Wrapper>
+    </>
+  )
+}
 
 export default JoinUsPage
