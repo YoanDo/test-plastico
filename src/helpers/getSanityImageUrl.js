015085da601978f413985f747ffc14aa0a4679fc
Yo <y.n.dorny@gmail.com>
2022-11-09 19:39:13 +0100

    feat(sanity): add sanity

---
+++ src/helpers/getSanityImageUrl.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import imageUrlBuilder from '@sanity/image-url'
+import sanityClient from '../../client'
+
+const builder = imageUrlBuilder(sanityClient)
+
+function getSanityImageUrl(source) {
+  return builder.image(source)
+}
+
+export default getSanityImageUrl
