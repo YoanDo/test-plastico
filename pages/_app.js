573d3715a93590c6ef4260ba05878c83c1387037
Yo <y.n.dorny@gmail.com>
2022-12-19 10:28:01 +0100

    refactor(meta): document --> app

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,24 @@
-import { IntlProvider } from 'react-intl'
-import { Provider } from 'react-redux'
-import { ThemeProvider } from 'styled-components'
-import App from 'next/app'
-import Head from 'next/head'
-import React from 'react'
-import { createWrapper } from 'next-redux-wrapper'
+import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import App from 'next/app';
+import Head from 'next/head';
+import React from 'react';
+import { createWrapper } from 'next-redux-wrapper';
 
-import BurgerMenu from '../src/components/Menu/BurgerMenu'
-import store from '../src/redux/store'
-import theme from '../src/assets/theme'
-import translationEn from '../src/assets/translations/en'
-import translationFr from '../src/assets/translations/fr'
-import RootModal from '../src/containers/RootModal'
+import BurgerMenu from '../src/components/Menu/BurgerMenu';
+import store from '../src/redux/store';
+import theme from '../src/assets/theme';
+import translationEn from '../src/assets/translations/en';
+import translationFr from '../src/assets/translations/fr';
+import RootModal from '../src/containers/RootModal';
 
 const translations = {
   fr: translationFr,
-  en: translationEn,
-}
+  en: translationEn
+};
 
-let language = 'fr'
+let language = 'fr';
 
 if (
   typeof window !== 'undefined' &&
@@ -26,39 +26,53 @@ if (
   navigator.language &&
   navigator.language.split(/[-_]/)[0] !== 'fr'
 ) {
-  language = 'en'
+  language = 'en';
 }
 
 class MyApp extends App {
   render() {
-    const { Component, appProps } = this.props
+    const { Component, appProps } = this.props;
 
     return (
-      <>
-        <Provider store={store}>
-          <Head>
-            <title>Plastic Origins</title>
-            <link
-              rel="stylesheet"
-              href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
-            />
-          </Head>
-          <IntlProvider locale={language} messages={translations[language]}>
-            <ThemeProvider theme={theme}>
-              <BurgerMenu pageWrapId="page-wrap" />
-              <main id="page-wrap">
-                <RootModal />
-                <Component {...appProps} />
-              </main>
-            </ThemeProvider>
-          </IntlProvider>
-        </Provider>
-      </>
-    )
+      <Provider store={store}>
+        <Head>
+          <title>Plastic Origins</title>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+          />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link rel="canonical" href="www.plastic-origins.eu" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta
+            name="description"
+            content="A citizen science project that uses AI to map river plastic pollution – powered by Surfrider Foundation Europe"
+          />
+          <meta property="og:title" content="Plastic Origins" key="title" />
+          <meta name="google-play-app" content="com.plasticorigins" />
+          <meta name="apple-itunes-app" content="app-id=1532710998" />
+        </Head>
+        <IntlProvider locale={language} messages={translations[language]}>
+          <ThemeProvider theme={theme}>
+            <BurgerMenu pageWrapId="page-wrap" />
+            <main id="page-wrap">
+              <RootModal />
+              <Component {...appProps} />
+            </main>
+          </ThemeProvider>
+        </IntlProvider>
+      </Provider>
+    );
   }
 }
 
-const makeStore = () => store
-const wrapper = createWrapper(makeStore)
+const makeStore = () => store;
+const wrapper = createWrapper(makeStore);
 
-export default wrapper.withRedux(MyApp)
+export default wrapper.withRedux(MyApp);
--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,21 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-import React from 'react'
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
 // Import styled components ServerStyleSheet
-import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />)
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -24,9 +24,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         )
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 
@@ -38,22 +38,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Bebas+Neue|Montserrat:300,400&display=swap"
             rel="stylesheet"
           />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <link rel="canonical" href="www.plastic-origins.eu" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <meta
-            name="description"
-            content="A citizen science project that uses AI to map river plastic pollution – powered by Surfrider Foundation Europe"
-          />
-          <meta property="og:title" content="Plastic Origins" key="title" />
-          <meta name="google-play-app" content="com.plasticorigins" />
-          <meta name="apple-itunes-app" content="app-id=1532710998" />
           <link rel="shortcut icon" href="/favicon.ico" />
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
@@ -65,6 +49,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
