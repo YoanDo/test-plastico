da0f98148a9e1e4dfdc9d9329c45a6f22418dab7
Yo <y.n.dorny@gmail.com>
2023-03-11 18:39:28 +0100

    refactor(document): modern way
    getInitialProps is now defined as a static method on the function component, rather than a static method on the class component. The sheet is now passed to getInitialProps via the props parameter, rather than being declared in the function body. Finally, getInitialProps now returns styleTags as a separate prop, which is used in the Head component to output the styles.

--- pages/_document.js
+++ pages/_document.js
@@ -1,59 +1,54 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
-// Import styled components ServerStyleSheet
 import { ServerStyleSheet } from 'styled-components';
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />)
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=Bebas+Neue|Montserrat:300,400&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          {/* Step 5: Output the styles in the head  */}
-          {this.props.styleTags}
-        </Head>
-        {/* TO FIX NEXTJS FOUC IN PROD */}
-        <body>
-          <script>0</script>
-          <Main />
-          <NextScript />
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src=https://www.googletagmanager.com/ns.html?id=GTM-TGNRLHV" height="0" width="0" style="display: none; visibility: hidden;" />`
-            }}
-          />
-        </body>
-      </Html>
-    );
-  }
+export default function MyDocument(props) {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Bebas+Neue|Montserrat:300,400&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        {props.styleTags}
+      </Head>
+      <body>
+        <script>0</script>
+        <Main />
+        <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src=https://www.googletagmanager.com/ns.html?id=GTM-TGNRLHV" height="0" width="0" style="display: none; visibility: hidden;" />`
+          }}
+        />
+      </body>
+    </Html>
+  );
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+      styleTags: sheet.getStyleElement()
+    };
+  } finally {
+    sheet.seal();
+  }
+};
