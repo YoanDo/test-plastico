c84f46c141439a4266e40309962ec4d085842a3e
Yo <y.n.dorny@gmail.com>
2023-06-20 11:25:54 +0200

    Revert "fix(map-trash): prevent missing style error in map"
    This reverts commit a651275cfcb2c235624ed32ecf2e439eeb94f2be.

--- src/components/MapRough/Map.js
+++ src/components/MapRough/Map.js
@@ -1,138 +1,61 @@
-import { useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
-import useFetchData from './FetchData';
-import heatmapConfig from '../../assets/mapAssets/TrashHeatmapStyle';
-import circleConfig from '../../assets/mapAssets/TrashCircleStyle';
-import circleHighlightConfig from '../../assets/mapAssets/TrashCircleHighlightStyle';
-import circleBackgroundConfig from '../../assets/mapAssets/TrashCircleBackgroundStyle';
-import { getTypeName } from '../../assets/mapAssets/TypeId';
+import TrashLayer from './TrashLayer';
 
-const TrashLayer = ({ map, url }) => {
-  const [data, loading] = useFetchData(url);
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2FiaW5lYWxsb3VzdXJmcmlkZXIiLCJhIjoiY2xnZXY3NWFpMHoyaDNtcDhrYWZscGJ1ZCJ9.I21vKjTW3QIyuwfb19HhDg';
 
-  const addLayers = () => {
-    if (!data || !map) return;
-
-    // Adding source
-    let source = map.getSource('data');
-    if (!source) {
-      map.addSource('data', {
-        type: 'geojson',
-        data
-      });
-      source = map.getSource('data');
-    } else {
-      source.setData(data);
-    }
-
-    // Adding heatmap layer
-    if (!map.getLayer('heatmap_trash')) {
-      map.addLayer({
-        id: 'heatmap_trash',
-        type: 'heatmap',
-        source: 'data',
-        maxzoom: 17,
-        paint: heatmapConfig
-      });
-    }
-
-    // Adding points layer
-    if (!map.getLayer('circle_trash')) {
-      map.addLayer({
-        id: 'circle_trash',
-        type: 'circle',
-        source: 'data',
-        minzoom: 13,
-        layout: { visibility: 'visible' },
-        paint: circleConfig
-      });
-    }
-
-    // Adding a layer that makes non-selected points greyish
-    if (!map.getLayer('circle_trash_background')) {
-      map.addLayer({
-        id: 'circle_trash_background',
-        type: 'circle',
-        source: 'data',
-        minzoom: 13,
-        paint: circleBackgroundConfig,
-        layout: { visibility: 'none' }
-      });
-    }
-
-    // Adding a layer that highlights points of the same campaign as the clicked point
-    if (!map.getLayer('circle_trash_highlight')) {
-      map.addLayer({
-        id: 'circle_trash_highlight',
-        type: 'circle',
-        source: 'data',
-        minzoom: 13,
-        paint: circleHighlightConfig,
-        filter: ['==', 'id_ref_campaign_fk', '']
-      });
-    }
-  };
-
-  const addClickEffects = () => {
-    map.on('click', 'circle_trash', (event) => {
-      const feature = event.features[0];
-      const typeName = getTypeName(feature.properties.type_id);
-
-      new mapboxgl.Popup()
-        .setLngLat(feature.geometry.coordinates)
-        .setHTML(
-          `
-          <strong>ID:</strong> ${feature.properties.id}
-          <p><strong>Campaign: </strong>${feature.properties.id_ref_campaign_fk}</p>
-          <p><strong>Type: </strong>${typeName}</p>
-          <p><strong>Date: </strong>${feature.properties.time}</p>
-          <p><strong>River: </strong>${feature.properties.river_name}</p>
-        `
-        )
-        .addTo(map);
-
-      const clickedCampaign = feature.properties.id_ref_campaign_fk;
-      if (map.setFilter) {
-        map.setFilter('circle_trash_highlight', [
-          '==',
-          'id_ref_campaign_fk',
-          clickedCampaign
-        ]);
-      }
-
-      if (map.setLayoutProperty) {
-        map.setLayoutProperty(
-          'circle_trash_background',
-          'visibility',
-          'visible'
-        );
-      }
-    });
-  };
-
-  const removeClickEffects = () => {
-    const highlightLayerExists = map.getLayer('circle_trash_highlight');
-    if (highlightLayerExists) {
-      map.removeLayer('circle_trash_highlight');
-      map.removeSource('circle_trash_highlight');
-    }
-
-    map.setLayoutProperty('circle_trash_background', 'visibility', 'none');
-  };
+const Map = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(2.1); // -1.0
+  const [lat, setLat] = useState(46.1); // 43.47
+  const [zoom, setZoom] = useState(5); // 14
 
   useEffect(() => {
-    if (loading) return;
+    if (map.current) return;
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom,
+      minZoom: 4,
+      pitch: 0,
+      bearing: 0
+    });
 
-    addLayers();
-    addClickEffects();
+    map.current.addControl(new mapboxgl.FullscreenControl());
+    map.current.addControl(new mapboxgl.NavigationControl());
+
+    map.current.on('load', () => {
+      console.log('Map is loaded.');
+      map.current.resize();
+    });
 
     return () => {
-      map.off('click', 'circle_trash');
-      removeClickEffects();
+      map.current.remove();
     };
-  }, [data, loading]);
+  }, [lat, lng, zoom]);
 
-  return null;
+  useEffect(() => {
+    if (!map.current) return;
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  }, [map]);
+
+  const url = `https://api-plastico-prod.azurewebsites.net/v1/geojson/-8.0/33.0/28.0/66.0?entity_type=trash`;
+  return (
+    <>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+      {map.current && <TrashLayer url={url} map={map.current} />}
+    </>
+  );
 };
 
-export default TrashLayer;
+export default Map;
