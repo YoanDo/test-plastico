b2305d6ca14d25dc1dfe28549579d0d29619e63a
Yo <y.n.dorny@gmail.com>
2023-06-23 10:23:05 +0200

    refactor(trash-map): unused value & clean

--- src/components/MapRough/TrashLayer.js
+++ src/components/MapRough/TrashLayer.js
@@ -7,122 +7,125 @@ import circleHighlightConfig from '../../assets/mapAssets/TrashCircleHighlightSt
 import circleBackgroundConfig from '../../assets/mapAssets/TrashCircleBackgroundStyle';
 import { getTypeName } from '../../assets/mapAssets/TypeId';
 
-const TrashLayer = ({ map, url }) => {
-  const [data, loading] = useFetchData(url);
+const TrashLayer = (props) => {
+  const [data] = useFetchData(props.url);
 
   useEffect(() => {
-    if (!map || !data) return;
+    if (!data || !props.map) return;
 
-    const addLayers = () => {
-      // Adding source
-      let source = map.getSource('data');
-      if (!source) {
-        map.addSource('data', {
-          type: 'geojson',
-          data
-        });
-        source = map.getSource('data');
-      } else {
-        source.setData(data);
-      }
-
-      // Adding heatmap layer
-      if (!map.getLayer('heatmap_trash')) {
-        map.addLayer({
-          id: 'heatmap_trash',
-          type: 'heatmap',
-          source: 'data',
-          maxzoom: 17,
-          paint: heatmapConfig
-        });
-      }
-
-      // Adding points layer
-      if (!map.getLayer('circle_trash')) {
-        map.addLayer({
-          id: 'circle_trash',
-          type: 'circle',
-          source: 'data',
-          minzoom: 13,
-          layout: { visibility: 'visible' },
-          paint: circleConfig
-        });
-      }
-
-      // Adding a layer that makes non-selected points greyish
-      if (!map.getLayer('circle_trash_background')) {
-        map.addLayer({
-          id: 'circle_trash_background',
-          type: 'circle',
-          source: 'data',
-          minzoom: 13,
-          paint: circleBackgroundConfig,
-          layout: { visibility: 'none' }
-        });
-      }
-
-      // Adding a layer that highlights points of the same campaign as the clicked point
-      if (!map.getLayer('circle_trash_highlight')) {
-        map.addLayer({
-          id: 'circle_trash_highlight',
-          type: 'circle',
-          source: 'data',
-          minzoom: 13,
-          paint: circleHighlightConfig,
-          filter: ['==', 'id_ref_campaign_fk', '']
-        });
-      }
-    };
-
-    const addClickEffects = () => {
-      map.on('click', 'circle_trash', (event) => {
-        const feature = event.features[0];
-        const typeName = getTypeName(feature.properties.type_id);
-
-        new mapboxgl.Popup()
-          .setLngLat(feature.geometry.coordinates)
-          .setHTML(
-            `
-            <strong>ID:</strong> ${feature.properties.id}
-            <p><strong>Campaign: </strong>${feature.properties.id_ref_campaign_fk}</p>
-            <p><strong>Type: </strong>${typeName}</p>
-            <p><strong>Date: </strong>${feature.properties.time}</p>
-            <p><strong>River: </strong>${feature.properties.river_name}</p>
-          `
-          )
-          .addTo(map);
-
-        const clickedCampaign = feature.properties.id_ref_campaign_fk;
-        if (map.setFilter) {
-          map.setFilter('circle_trash_highlight', [
-            '==',
-            'id_ref_campaign_fk',
-            clickedCampaign
-          ]);
-        }
-
-        if (map.setLayoutProperty) {
-          map.setLayoutProperty(
-            'circle_trash_background',
-            'visibility',
-            'visible'
-          );
-        }
+    // Adding or updating the source
+    const sourceId = 'data';
+    if (!props.map.getSource(sourceId)) {
+      props.map.addSource(sourceId, {
+        type: 'geojson',
+        data
       });
+    } else {
+      props.map.getSource(sourceId).setData(data);
+    }
+
+    // Adding heatmap layer
+    const heatmapLayerId = 'heatmap_trash';
+    if (!props.map.getLayer(heatmapLayerId)) {
+      props.map.addLayer({
+        id: heatmapLayerId,
+        type: 'heatmap',
+        source: sourceId,
+        maxzoom: 17,
+        paint: heatmapConfig
+      });
+    }
+
+    // Adding points layer
+    const circleLayerId = 'circle_trash';
+    if (!props.map.getLayer(circleLayerId)) {
+      props.map.addLayer({
+        id: circleLayerId,
+        type: 'circle',
+        source: sourceId,
+        minzoom: 13,
+        layout: { visibility: 'visible' },
+        paint: circleConfig
+      });
+    }
+
+    // Adding a layer that makes non-selected points greyish
+    const circleBackgroundLayerId = 'circle_trash_background';
+    if (!props.map.getLayer(circleBackgroundLayerId)) {
+      props.map.addLayer({
+        id: circleBackgroundLayerId,
+        type: 'circle',
+        source: sourceId,
+        minzoom: 13,
+        paint: circleBackgroundConfig,
+        layout: { visibility: 'none' }
+      });
+    }
+
+    // Adding a layer that highlights points of the same campaign as the clicked point
+    const circleHighlightLayerId = 'circle_trash_highlight';
+    if (!props.map.getLayer(circleHighlightLayerId)) {
+      props.map.addLayer({
+        id: circleHighlightLayerId,
+        type: 'circle',
+        source: sourceId,
+        minzoom: 13,
+        paint: circleHighlightConfig,
+        filter: ['==', 'id_ref_campaign_fk', '']
+      });
+    }
+
+    const removeClickEffects = () => {
+      props.map.setFilter(circleHighlightLayerId, [
+        '==',
+        'id_ref_campaign_fk',
+        ''
+      ]);
+      props.map.setLayoutProperty(
+        circleBackgroundLayerId,
+        'visibility',
+        'none'
+      );
     };
 
-    addLayers();
-    addClickEffects();
+    // Adding effects on click
+    const onClickCircleTrash = (event) => {
+      const feature = event.features[0];
+      const typeName = getTypeName(feature.properties.type_id);
+      new mapboxgl.Popup()
+        .setLngLat(feature.geometry.coordinates)
+        .setHTML(
+          `<strong>ID:</strong> ${feature.properties.id}
+        <p><strong>Campaign: </strong>${feature.properties.id_ref_campaign_fk}</p>
+        <p><strong>Type: </strong>${typeName}</p>
+        <p><strong>Date: </strong>${feature.properties.time}</p>
+        <p><strong>River: </strong>${feature.properties.river_name}</p>
+        `
+        )
+        .addTo(props.map);
 
+      const clickedCampaign = feature.properties.id_ref_campaign_fk;
+      props.map.setFilter(circleHighlightLayerId, [
+        '==',
+        'id_ref_campaign_fk',
+        clickedCampaign
+      ]);
+
+      props.map.setLayoutProperty(
+        circleBackgroundLayerId,
+        'visibility',
+        'visible'
+      );
+    };
+
+    props.map.on('click', 'circle_trash', onClickCircleTrash);
+
+    // Removing effects
     return () => {
-      map.off('click', 'circle_trash');
-      map.removeLayer('circle_trash_highlight');
-      map.removeLayer('circle_trash_background');
-      map.removeLayer('circle_trash');
-      map.removeLayer('heatmap_trash');
-      map.removeSource('data');
+      props.map.off('click', 'circle_trash', onClickCircleTrash);
+      removeClickEffects();
     };
-  }, [map, data]);
+  }, [data, props.map]);
 
   return null;
 };
