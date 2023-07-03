70c0a341a719b87b2b109cce6a0a845ade2d3c9c
Yo <y.n.dorny@gmail.com>
2023-07-04 00:06:06 +0200

    wip(data-page): reorg

--- src/scenes/Data/index.js
+++ src/scenes/Data/index.js
@@ -3,13 +3,20 @@ import { array } from 'prop-types';
 import Map from '../../components/MapRough/Map';
 import DataHeader from '../../components/DataHeader2';
 import DataFaq from '../../containers/DataFaq';
+import { Wrapper } from '../../hoc/layout/styles';
+import { Title } from './styles';
 
 const DataPage = ({ faqs }) => (
   <>
-    - header
     <DataHeader />
-    <Map />- faq - data source
-    <DataFaq faqs={faqs} />
+    <Map />- _lien vers source_
+    <Wrapper>
+      <Title>questions</Title>
+      <DataFaq faqs={faqs} />
+    </Wrapper>
+    <Wrapper>
+      <Title>source</Title>
+    </Wrapper>
   </>
 );
 
