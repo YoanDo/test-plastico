92869e6f0314484105003703d97fb4281a27b5b4
Yo <y.n.dorny@gmail.com>
2023-09-11 13:51:07 +0200

    refactor(types): lint

--- src/assets/mapAssets/TypeId.js
+++ src/assets/mapAssets/TypeId.js
@@ -1,48 +1,48 @@
 export function getTypeName(typeId) {
   switch (typeId) {
     case 1:
-      return 'AutoFragment'; //'Fragment plastique'; //'Sheet / tarp / plastic bag / fragment';
+      return 'AutoFragment'; // 'Fragment plastique'; //'Sheet / tarp / plastic bag / fragment';
     case 2:
-      return 'AutoInsulating'; //'Matériau isolant'; //'Insulating material';
+      return 'AutoInsulating'; // 'Matériau isolant'; //'Insulating material';
     case 3:
-      return 'AutoBottleShaped'; //'Bouteille'; //'Bottle-shaped';
+      return 'AutoBottleShaped'; // 'Bouteille'; //'Bottle-shaped';
     case 4:
-      return 'AutoCanShaped'; //'Canette'; //'Can-shaped';
+      return 'AutoCanShaped'; // 'Canette'; //'Can-shaped';
     case 5:
-      return 'AutoDrum'; //'Rouleau'; //'Drum';
+      return 'AutoDrum'; // 'Rouleau'; //'Drum';
     case 6:
-      return 'AutoOtherPackaging'; //'Autre emballage'; //'Other packaging';
+      return 'AutoOtherPackaging'; // 'Autre emballage'; //'Other packaging';
     case 7:
-      return 'AutoTire'; //'Pneu'; //'Tire';
+      return 'AutoTire'; // 'Pneu'; //'Tire';
     case 8:
-      return 'AutoFishingNet'; //'Filet'; //'Fishing net / cord';
+      return 'AutoFishingNet'; // 'Filet'; //'Fishing net / cord';
     case 9:
-      return 'AutoEasilyNamable'; //'Facilement nommable'; //'Easily namable';
+      return 'AutoEasilyNamable'; // 'Facilement nommable'; //'Easily namable';
     case 10:
-      return 'AutoUnclear'; //'Indéterminé'; //'Unclear';
+      return 'AutoUnclear'; // 'Indéterminé'; //'Unclear';
     case 11:
-      return 'Fragment'; //'Fragment plastique'; //'Sheet / tarp / plastic bag / fragment';
+      return 'Fragment'; // 'Fragment plastique'; //'Sheet / tarp / plastic bag / fragment';
     case 12:
-      return 'AgriculturalFoodWaste'; 
+      return 'AgriculturalFoodWaste';
     case 13:
-      return 'Bottles'; //'Bouteille';
+      return 'Bottles'; // 'Bouteille';
     case 14:
-      return 'Industrials'; 
+      return 'Industrials';
     case 15:
-      return 'FishHunting'; 
+      return 'FishHunting';
     case 16:
-      return 'FoodPackage'; 
+      return 'FoodPackage';
     case 17:
-      return 'HouseholdItems'; 
+      return 'HouseholdItems';
     case 18:
-      return 'Fragment10'; 
+      return 'Fragment10';
     case 19:
-      return 'Trash'; 
+      return 'Trash';
     case 20:
-      return 'BulkyTrash';  
+      return 'BulkyTrash';
     case 21:
-      return 'AccumulationZone'; 
+      return 'AccumulationZone';
     default:
-      return 'Unknown'; //'Indéterminé';
+      return 'Unknown'; // 'Indéterminé';
   }
 }
