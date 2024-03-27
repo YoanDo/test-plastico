e019a6c649f5e7e41016f7dfd6990ca0035fcd5a
Yo <yoan.dorny@greenscope.io>
2024-03-27 20:45:39 +0100

    feat(google-tags): sets tracking

--- pages/_document.js
+++ pages/_document.js
@@ -2,53 +2,64 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
-export default function MyDocument(props) {
-  return (
-    <Html>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Bebas+Neue|Montserrat:300,400&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        {props.styleTags}
-      </Head>
-      <body>
-        <script>0</script>
-        <Main />
-        <NextScript />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src=https://www.googletagmanager.com/ns.html?id=GTM-TGNRLHV" height="0" width="0" style="display: none; visibility: hidden;" />`
-          }}
-        />
-      </body>
-    </Html>
-  );
-}
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-MyDocument.getInitialProps = async (ctx) => {
-  const sheet = new ServerStyleSheet();
-  const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />)
+        });
 
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      ),
-      styleTags: sheet.getStyleElement()
-    };
-  } finally {
-    sheet.seal();
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
   }
-};
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Bebas+Neue|Montserrat:300,400&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-TGNRLHV');
+              `
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGNRLHV" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          />
+        </body>
+      </Html>
+    );
+  }
+}
